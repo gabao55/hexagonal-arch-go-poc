e2b21794f9d0b819c2a50d6c9c5bc21a7806eb78
gabao55 <gabriel.s.rosin@gmail.com>
2024-07-08 20:54:44 -0300

    config: Sets imports

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -1,22 +1,22 @@
 package db
 
-import (
-	"database/sql"
-	"github.com/gabao55/hexagonal-arch-go/application"
-	_ "github.com/mattn/go-sqlite3"
-)
+// import (
+// 	"database/sql"
+// 	"github.com/gabao55/hexagonal-arch-go/application"
+// 	_ "github.com/mattn/go-sqlite3"
+// )
 
-type ProductDb struct {
-	db *sql.DB
-}
+// type ProductDb struct {
+// 	db *sql.DB
+// }
 
-func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
-	var product application.Product
-	stmt, err := p.db.Prepare("SELECT id, name, price, status FROM products WHERE id = ?")
-	if err != nil {
-		return nil, err
-	}
-	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
+// func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
+// 	var product application.Product
+// 	stmt, err := p.db.Prepare("SELECT id, name, price, status FROM products WHERE id = ?")
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
 
-	return &product, nil
-}
+// 	return &product, nil
+// }
