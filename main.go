f86d30eac241dea8b6549fdaf02e5deb834357f6
gabao55 <gabriel.s.rosin@gmail.com>
2024-07-09 12:07:16 -0300

    chore: Implements cobra CLI adapter

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"database/sql"
-	"github.com/gabao55/hexagonal-arch-go/application"
-	"github.com/gabao55/hexagonal-arch-go/adapters/db"
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/gabao55/hexagonal-arch-go/cmd"
 
 func main() {
-	Db, _ := sql.Open("sqlite3", "db.sqlite3")
-	productDbAdapter := db.NewProductDb(Db)
-	productService := application.NewProductService(productDbAdapter)
-
-	product, _ := productService.Create("Example Product", 50)
-	productService.Enable(product)
+	cmd.Execute()
 }
